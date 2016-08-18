382876e896510951ff9fb5539e28d889d23c0721
steven.a.chan <steven.a.chan@gov.ab.ca>
2016-08-18 16:11:35 -0600

    Fixed \n not breaking. Added image upload functionality. Added link button functionality - Image link working.

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ $(document).ready(function () {
     $("#option1").click(function () {
         $("#option-button").html('It would be nice if &nbsp;<span class="caret"></span>');
         $("#text-area").attr("placeholder", nice_text + "...").val("").focus();
+        $("#comment-text").attr("placeholder", nice_text + "...").val("");
         option = 1;
     });
     $("#option2").click(function () {
         $("#option-button").html('It would be horrible if &nbsp;<span class="caret"></span>');
         $("#text-area").attr("placeholder", horrible_text + "...").val("").focus();
+        $("#comment-text").attr("placeholder", horrible_text + "...").val("");
         option = 2;
     });
 
@@ -41,28 +43,28 @@ $(document).ready(function () {
             });
         }
     });
-    
-    
+
+
     $("#cancel-button").mousedown(function () {
         $("#text-area").val('');
         $("#text-area").autoHeight()
     });
 
     $("#submit-button").mousedown(function () {
-        var input_text = $('#text-area').val();
+        var input_text = $('#text-area').val().replace(/\n/g, '<br />');
         var date = new Date(Date.now());
         var result = new Array();
         result[0] = $.datepicker.formatDate('DD M d yy', date);
         result[1] = ' ';
         if (date.getHours() > 12) {
             result[2] = date.getHours() - 12;
-        } else if (date.getHours() == 0 ) {
+        } else if (date.getHours() == 0) {
             result[2] = "12";
         } else {
             result[2] = date.getHours();
         }
         result[3] = ":";
-        result[4] = (date.getMinutes()<10?'0':'') + date.getMinutes();
+        result[4] = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
 
         if (date.getHours() > 12) {
             result[5] = " pm";
@@ -70,22 +72,20 @@ $(document).ready(function () {
             result[5] = " am";
         }
         var combined_time = "";
-        $.each(result,function(key,value){
+        $.each(result, function (key, value) {
             //combined_time+=value;
             combined_time += result[key];
         });
         //var sd = $("#text-area").attr('placeholder');
         //var ds = $("#option-button").html();
         //$('label[for^="'+sd+'"]').fadeIn();
-        if(option==1){
+        if (option == 1) {
             chosen = nice_text;
             $("<div class='panel panel-lightblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
-        }else if (option==2){
+        } else if (option == 2) {
             chosen = horrible_text;
             $("<div class='panel panel-darkblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
         }
-
-
 
         $("#text-area").val('');
         $("#text-area").autoHeight()
@@ -94,16 +94,146 @@ $(document).ready(function () {
     $("#text-area").on("focus", function (e) {
         $('#submit-button').show();
         $('#cancel-button').show();
-        $('#file-upload').prop("disabled", false);
-        $('#link-upload').prop("disabled", false);
     });
 
     $("#text-area").on("blur", function (e) {
         $('#submit-button').hide();
         $('#cancel-button').hide();
-        //$('#file-upload').prop("disabled",true);
+    });
+    $(document).on('hide.bs.modal', '#myModal', function () {
+        $("#comment-text").val('');
+        $("#comment-text").autoHeight();
     });
 
+    $("#submit-image-button").mousedown(function () {
+        var input_text = $('#comment-text').val().replace(/\n/g, '<br />');
+        var date = new Date(Date.now());
+        var result = new Array();
+        result[0] = $.datepicker.formatDate('DD M d yy', date);
+        result[1] = ' ';
+        if (date.getHours() > 12) {
+            result[2] = date.getHours() - 12;
+        } else if (date.getHours() == 0) {
+            result[2] = "12";
+        } else {
+            result[2] = date.getHours();
+        }
+        result[3] = ":";
+        result[4] = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+
+        if (date.getHours() > 12) {
+            result[5] = " pm";
+        } else {
+            result[5] = " am";
+        }
+        var combined_time = "";
+        $.each(result, function (key, value) {
+            combined_time += result[key];
+        });
+        if (option == 1) {
+            chosen = nice_text;
+            $("<div class='panel panel-lightblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+        } else if (option == 2) {
+            chosen = horrible_text;
+            $("<div class='panel panel-darkblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+        }
+
+        $('#myModal').modal('hide');
+        $("#text-area").val('');
+        $("#text-area").autoHeight()
+    });
+
+    $("#attach-link").click(function () {
+        $("#myModal-link").modal('show');
+    });
+
+    $(document).on('hide.bs.modal', '#myModal-link', function () {
+        $("#url-link").val('');
+        $("#comment-text-link").val('');
+        $("#comment-text").autoHeight();
+    });
+
+    $("#submit-button-link").mousedown(function () {
+        var input_url = $('#url-link').val();
+        var input_text = $('#comment-text-link').val().replace(/\n/g, '<br />');
+
+        var image_extensions = ['gif', 'jpeg', 'jpg', 'png', 'bmp', 'svg', 'webp'];
+        var image_extensions_length = image_extensions.length;
+
+        var isImage = false;
+        while (image_extensions_length--) {
+            if (input_url.indexOf(image_extensions[image_extensions_length]) != -1) {
+                console.log(image_extensions[image_extensions_length] + ":true");
+                isImage = true;
+                break;
+            }
+        }
+        var isVideo = false;
+        if (isImage == false) {
+            var video_extensions = ['youtube'];
+            var video_extensions_length = video_extensions.length;
+            while (video_extensions_length--) {
+                if (input_url.indexOf(video_extensions[video_extensions_length]) != -1) {
+                    console.log("true");
+                    isVideo = true;
+                    break;
+                }
+            }
+        }
+
+        var date = new Date(Date.now());
+        var result = new Array();
+        result[0] = $.datepicker.formatDate('DD M d yy', date);
+        result[1] = ' ';
+        if (date.getHours() > 12) {
+            result[2] = date.getHours() - 12;
+        } else if (date.getHours() == 0) {
+            result[2] = "12";
+        } else {
+            result[2] = date.getHours();
+        }
+        result[3] = ":";
+        result[4] = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+
+        if (date.getHours() > 12) {
+            result[5] = " pm";
+        } else {
+            result[5] = " am";
+        }
+        var combined_time = "";
+        $.each(result, function (key, value) {
+            combined_time += result[key];
+        });
+
+        if (isImage == true) {
+            if (option == 1) {
+                chosen = nice_text;
+                $("<div class='panel panel-lightblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'><img src='" +input_url + "' alt='your image' style='height: auto; width: auto; max-width: 400px;max-height: 400px; border:2px solid gainsboro; background-color: white; border-radius: 8px; box-sizing: border-box; display: block; margin: 0 auto' /><br/>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+            } else if (option == 2) {
+                chosen = horrible_text;
+                $("<div class='panel panel-darkblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+            }
+        } else if (isVideo == true) {
+            if (option == 1) {
+                chosen = nice_text;
+                $("<div class='panel panel-lightblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+            } else if (option == 2) {
+                chosen = horrible_text;
+                $("<div class='panel panel-darkblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+            }
+        } else {
+            if (option == 1) {
+                chosen = nice_text;
+                $("<div class='panel panel-lightblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+            } else if (option == 2) {
+                chosen = horrible_text;
+                $("<div class='panel panel-darkblue'><div class='panel-heading'><div class='panel-title pull-left' style='font-size: 25px'>" + chosen + "</div><div class='panel-title pull-right'><h5><i>" + combined_time + "</i></h5></div><div class='clearfix'></div></div><div class='panel-body'>" + input_text + "</div><div class='panel-footer'>Panel footer</div></div>").appendTo("#card-body");
+            }
+        }
+        $('#myModal-link').modal('hide');
+        $("#text-area").val('');
+        $("#text-area").autoHeight()
+    });
 });
 
 function readURL(input) {
@@ -111,12 +241,17 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#blah')
-                .attr('src', e.target.result)
-                .width(200)
-                .height(200);
+            $('#image-file')
+                .attr('src', e.target.result);
+            //.width(100)
+            //.height(200);
         };
 
         reader.readAsDataURL(input.files[0]);
     }
+    //var filename = input.val();
+    var filename = $('input[type=file]').val().split('\\').pop();
+    $('#image-file-name').text(filename);
+    $('#myModal').modal('toggle');
 }
+
